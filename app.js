a1aa1bfb951c5556c0dfca8a57a7689f5bd036aa
artyomtrus <trusartyom@yandex.by>
2022-10-24 21:31:25 +0300

    диплом проверка

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/error-handler');
 const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const limiter = require('./utils/limiter');
 
 const { PORT = 3000, DB_ADDRESS = 'mongodb://localhost:27017/moviesdb' } = process.env;
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 1000,
-});
+const allowedCors = [
+  'https://filmopoisk.trus.nomoredomains.icu/',
+  'http://filmopoisk.trus.nomoredomains.icu/',
+];
 
 const app = express();
 
+app.use(requestLogger);
+app.use(helmet());
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.use(limiter);
 
 mongoose.connect(DB_ADDRESS, {
   useNewUrlParser: true,
 });
-
-app.use(requestLogger);
 
 app.use(router);
 
